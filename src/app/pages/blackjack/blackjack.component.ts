09d47aa25d503ae6a72a36bdaa0c965cf25f6797
Wilder Canales <74937664+Inogamy@users.noreply.github.com>
2024-02-05 10:50:53 -0500

    Primeras funciones

--- src/app/pages/blackjack/blackjack.component.ts
+++ src/app/pages/blackjack/blackjack.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { shuffle } from 'underscore';
 
-
-
-
 @Component({
   selector: 'app-blackjack',
   standalone: true,
@@ -15,15 +12,19 @@ export class BlackjackComponent {
   deck: string[] = [];
   tipos = ['C', 'D', 'H', 'S'];
   especiales = ['A', 'J', 'Q', 'F']
+  deckshuffle: string[] = [];
+
 
   constructor() {
     this.crearDeck();
-    console.log('La baraja', this.deck);
-    this.deck = shuffle(this.deck);
-    console.log(this.deck)
+    console.log(`La baraja ${this.deck}`);
+    this.deckshuffle = shuffle(this.deck);
+    console.log(`La baraja combinada ${this.deckshuffle}`)
+    this.pedirCarta()
+    this.valorCarta('JD');
   }
 
-  crearDeck = () => {
+  crearDeck(){
     for (let i = 2; i <= 10; i++) {
       for (let tipo of this.tipos) {
         this.deck.push(i + tipo)
@@ -37,4 +38,28 @@ export class BlackjackComponent {
     }
   }
 
-}
+  pedirCarta(){
+    let carta = this.deckshuffle.pop()
+    console.log(`la carta es: ${carta}`)
+    console.log(`la carta es: ${this.deckshuffle}`)
+  }
+
+  valorCarta(carta: string){
+    let valor = carta.substring(0, carta.length - 1)
+
+    let numeroValor = parseInt(valor, 10);
+
+    console.log({ valor })
+
+    return (isNaN(numeroValor)) ?
+      ( valor === 'A') ? 11 : 10
+      : numeroValor = numeroValor + 1
+
+    }
+
+
+
+  }
+
+
+
